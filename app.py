17c57f45962362bbaec347b4e8beccadbbeccd2f
irachi7661 <162087036+irachi7661@users.noreply.github.com>
2025-03-10 12:24:58 +0600

    Update app.py

--- app.py
+++ app.py
@@ -162,7 +162,7 @@ def ping():
 
 # keep_alive ফাংশন
 def keep_alive():
-    url = "https://your-app-url.com/ping"  # আপনার এপ্লিকেশনের URL দিয়ে পরিবর্তন করুন
+    url = "https://photo-upload-production-cd8b.up.railway.app/ping"  # আপনার এপ্লিকেশনের URL দিয়ে পরিবর্তন করুন
     while True:
         time.sleep(300)
         try:
